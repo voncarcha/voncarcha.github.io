221d19630c57d6dd88a0a04d8aa4d7a805c32e57
von <von@istackmanila.com>
2020-10-11 22:43:17 +0800

    final fix pokedex

---
+++ pokedex/precache-manifest.93b0a4a8aa4159b137089c4c621c304e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25dea19750966be03e8edc9e74c36b38",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "d9335788ad3640672d72",
+    "url": "/pokedex/static/css/2.e10332d9.chunk.css"
+  },
+  {
+    "revision": "6ecd71b14f8a6ce644b6",
+    "url": "/pokedex/static/css/main.349db70f.chunk.css"
+  },
+  {
+    "revision": "d9335788ad3640672d72",
+    "url": "/pokedex/static/js/2.a5e4fa0a.chunk.js"
+  },
+  {
+    "revision": "eaa7cd7e464a88af728d93cb1acfbceb",
+    "url": "/pokedex/static/js/2.a5e4fa0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ecd71b14f8a6ce644b6",
+    "url": "/pokedex/static/js/main.f8e28965.chunk.js"
+  },
+  {
+    "revision": "5d5b7317cf43a0b8925c",
+    "url": "/pokedex/static/js/runtime-main.2d94522c.js"
+  },
+  {
+    "revision": "5f8a4ead7a7b10543daeb8835076ea74",
+    "url": "/pokedex/static/media/pokeball.5f8a4ead.png"
+  }
+]);
--- pokedex/service-worker.js
+++ pokedex/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.09c5dd77434efa1e5f78941288946fcd.js"
+  "/pokedex/precache-manifest.93b0a4a8aa4159b137089c4c621c304e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/pokedex/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
