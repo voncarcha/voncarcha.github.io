f3205dde88ec26d18f92fea8c2f63501c39abb0f
von <von@istackmanila.com>
2020-10-11 16:57:08 +0800

    added pokedex

---
+++ pokedex/precache-manifest.e27608b4b063f32cac8a5ec96693fa4f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cf3708fc843d51457e90f9bdacb2883",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2f91186a30b4d0ee607c",
+    "url": "/static/css/2.220ccb42.chunk.css"
+  },
+  {
+    "revision": "61630ec724523b47c69c",
+    "url": "/static/css/main.349db70f.chunk.css"
+  },
+  {
+    "revision": "2f91186a30b4d0ee607c",
+    "url": "/static/js/2.555a2014.chunk.js"
+  },
+  {
+    "revision": "eaa7cd7e464a88af728d93cb1acfbceb",
+    "url": "/static/js/2.555a2014.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61630ec724523b47c69c",
+    "url": "/static/js/main.24044556.chunk.js"
+  },
+  {
+    "revision": "e2fee617b0cce52d38a2",
+    "url": "/static/js/runtime-main.fde3a231.js"
+  },
+  {
+    "revision": "5f8a4ead7a7b10543daeb8835076ea74",
+    "url": "/static/media/pokeball.5f8a4ead.png"
+  }
+]);
